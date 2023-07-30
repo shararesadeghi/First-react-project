d0ca32d00ba7b075f1e0d22c73e158d6c0b6a449
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-07-30 15:26:50 +0430

    list renderin added to project

--- first-app/src/component/Cards.js
+++ first-app/src/component/Cards.js
@@ -8,14 +8,29 @@ import iphone14 from "../images/iphone14.jpg";
 
 
 class Cards extends Component {
+    constructor(){
+        super();
+        this.state = {
+            phoneData :[
+                {id : 1, image : iphonex, name : "Iphone X", cost : "500 $" },
+                {id : 2,image : iphone14pro, name : "Iphone 14pro", cost : "900 $" },
+                {id : 3,image : iphone, name : "Iphone", cost : "300 $" },
+                {id : 4,image : iphone14, name : "Iphone 14", cost : "600 $" }
+
+            ]
+        }
+    }
 
     render() {
         return (
             <div className={styles.container}>
+               {/*
                 <Card image={iphonex} name="Iphone X" cost="500 $"/>
                 <Card image={iphone14pro} name="Iphone 14pro" cost="900 $"/>
                 <Card image={iphone} name="Iphone" cost="300 $"/>
-                <Card image={iphone14} name="iphone 14" cost="600 $"/>
+               <Card image={iphone14} name="iphone 14" cost="600 $"/>*/}
+               {this.state.phoneData.map(phone => <Card key={phone.id} image={phone.image} name={phone.name} cost={phone.cost}/>)}
+                   
             </div>
         );
     }
